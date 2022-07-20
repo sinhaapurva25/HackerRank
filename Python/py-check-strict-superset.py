b8dd744c05e8c1ccff66de7cc9f0565779b34eee
Apurva Sinha <apurvasinha@4ctechventures.com>
2022-07-21 00:42:02 +0530

    adding files

--- Python/py-check-strict-superset.py
+++ Python/py-check-strict-superset.py
@@ -1,45 +1,10 @@
-# A = list(map(int,input().split()))
-# N = int(input())
-# N_arr = []
-# [N_arr.append(list(map(int,input().split()))) for i in range(N)]
-# output = 1
-# for i in range(N):
-
-#     if len(N_arr[i]) < len(A):
-#         output *=0
-#     else:
-#         count = 0
-#         for j in A:
-#             if j in N_arr[i]:
-#                 count += 1
-#         if count >= len(A):
-#             output *=1
-#         else:
-#             output *=0
-# print(bool(output))
-
-# ----------------------------------------------------------------
-
-A = list(map(int,input().split()))
+A = set(map(int,input().split()))
 N = int(input())
-
-N_arr = []
-
-[N_arr.append(list(map(int,input().split()))) for i in range(N)]
-
-output = 1
+b = 1
 for i in range(N):
-    count = 0
-
-    if len(N_arr[i]) < len(A):
-        output *=0
-
-    for j in A:
-        if j in N_arr[i]:
-            count += 1
-    if count == len(A):
-        output *=1
+    if A.issuperset(set(list(map(int,input().split())))):
+        b = 1
     else:
-        output *=0
-
-print(bool(output))
+        b = 0
+        break
+print(bool(b))
--- Python/validate-list-of-email-address-with-filter.py
+++ Python/validate-list-of-email-address-with-filter.py
@@ -1,8 +1,25 @@
 def fun(s):
-    username = s.split("@")[0]
-    websitename = s.split("@")[1].split(".")[0]
-    websitename = s.split("@")[1].split(".")[1]
     # return True if s is a valid email, else return False
+    idx_at_the_rate = s.find('@')
+    idx_dot = s.find('.')
+    # print(idx_at_the_rate, idx_dot)
+    if idx_at_the_rate > -1 and idx_dot > -1:
+        username = s[:idx_at_the_rate]
+        websitename = s[idx_at_the_rate+1:idx_dot]
+        extension  = s[idx_dot+1:]
+        # print(username,websitename,extension)
+        if username.isalnum() or username.find('_') > -1 or username.find('-') > -1:
+            if websitename.isalnum():
+                if extension.isalpha() and len(extension) <=3:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
 
 def filter_mail(emails):
     return list(filter(fun, emails))
